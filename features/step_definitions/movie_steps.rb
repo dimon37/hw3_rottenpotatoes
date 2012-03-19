c2a1d2c6752a054b25416851fe01122b328c09af
Vadim Dribinsky <dimon37@gmail.com>
2012-03-19 04:58:00 -0700

    got background tasks to work

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,10 +2,13 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    if Movie.find_by_title(movie[:title]) == nil
+      Movie.new(movie).save!
+      #puts movie[:title]
+    end
   end
-  assert false, "Unimplmemented"
+  ##Movie.all().save!
+  #puts Movie.all()
 end
 
 # Make sure that one string (regexp) occurs before or after another one
